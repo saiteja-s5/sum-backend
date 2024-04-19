237476579b56bb06735657565072ce5ffb2e886c
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-19 16:25:34 +0530

    Adding Eureka Server

---
+++ sum-service-registry/src/test/java/building/sum/service_registry/SumServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package building.sum.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
