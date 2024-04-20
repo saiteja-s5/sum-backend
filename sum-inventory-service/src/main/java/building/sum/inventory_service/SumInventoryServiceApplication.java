cb5ba8debdc66ff8548149d79ebeee62798b192f
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-20 14:53:38 +0530

    Added Inventory Microservice

---
+++ sum-inventory-service/src/main/java/building/sum/inventory_service/SumInventoryServiceApplication.java
@@ -0,0 +1,20 @@
+package building.sum.inventory_service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SumInventoryServiceApplication {
+
+	private static final Logger log = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumInventoryServiceApplication.class, args);
+		log.info("SUM Wealth Monitoring App Inventory Service started");
+	}
+
+}
---
+++ sum-inventory-service/src/test/java/building/sum/inventory_service/SumInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package building.sum.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
