6c4f377834f36bfbf181f01bdb7bdd918904a0e4
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-20 17:14:20 +0530

    Added Gateway Microservice

---
+++ sum-api-gateway/src/main/java/building/sum/api_gateway/SumApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package building.sum.api_gateway;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SumApiGatewayApplication {
+
+	private static final Logger log = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumApiGatewayApplication.class, args);
+		log.info("SUM Wealth Monitoring App API Gateway started");
+	}
+
+}
---
+++ sum-api-gateway/src/test/java/building/sum/api_gateway/SumApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package building.sum.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
