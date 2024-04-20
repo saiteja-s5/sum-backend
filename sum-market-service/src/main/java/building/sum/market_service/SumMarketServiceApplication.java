e0c03e91ed563b710cf6c24cf785912878798309
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-20 14:38:48 +0530

    Added Market Microservice

---
+++ sum-market-service/src/main/java/building/sum/market_service/SumMarketServiceApplication.java
@@ -0,0 +1,20 @@
+package building.sum.market_service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SumMarketServiceApplication {
+
+	private static final Logger log = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumMarketServiceApplication.class, args);
+		log.info("SUM Wealth Monitoring App Market Integration Service started");
+	}
+
+}
---
+++ sum-market-service/src/test/java/building/sum/market_service/SumMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package building.sum.market_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumMarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
