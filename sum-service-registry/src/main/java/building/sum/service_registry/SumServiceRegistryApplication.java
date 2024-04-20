49ea6565b810e8379e024c1aa9006cf1a9045590
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-20 12:18:35 +0530

    Log file Name in next line error fix

--- sum-service-registry/src/main/java/building/sum/service_registry/SumServiceRegistryApplication.java
+++ sum-service-registry/src/main/java/building/sum/service_registry/SumServiceRegistryApplication.java
@@ -1,5 +1,7 @@
 package building.sum.service_registry;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class SumServiceRegistryApplication {
 
+	private static final Logger log = LogManager.getLogger();
+
 	public static void main(String[] args) {
 		SpringApplication.run(SumServiceRegistryApplication.class, args);
+		log.info("SUM Wealth Monitoring App Eureka Server started");
 	}
 
 }
