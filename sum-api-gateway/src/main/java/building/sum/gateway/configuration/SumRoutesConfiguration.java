7963f9d15b56e7f292a7330bf28f553a1ec1453c
Saiteja Sabbani <saithejasabbani1@gmail.com>
2024-04-28 21:53:26 +0530

    Gateway Routes fix -> Configured as Java Class and remove common dependencies from parent

---
+++ sum-api-gateway/src/main/java/building/sum/gateway/configuration/SumRoutesConfiguration.java
@@ -0,0 +1,20 @@
+package building.sum.gateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SumRoutesConfiguration {
+
+	@Bean
+	RouteLocator sumMicroserviceRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("sum-inventory-service", r -> r.path("/stocks/**").uri("lb://sum-inventory-service"))
+				.route("sum-inventory-service", r -> r.path("/funds/**").uri("lb://sum-inventory-service"))
+				.route("sum-inventory-service", r -> r.path("/dividends/**").uri("lb://sum-inventory-service"))
+				.route("sum-market-service", r -> r.path("/daily-market/**").uri("lb://sum-market-service")).build();
+	}
+
+}
