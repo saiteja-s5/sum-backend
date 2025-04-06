56cd1bdd14e04da5e3e02b330f724bdc9476e7f6
Saiteja Sabbani <saithejasabbani1@gmail.com>
2025-04-06 18:07:50 +0530

    sum-report-service - daily after market report scheduler logging

--- sum-report-service/src/main/java/building/sum/report/listener/SumReportServiceListener.java
+++ sum-report-service/src/main/java/building/sum/report/listener/SumReportServiceListener.java
@@ -1,5 +1,7 @@
 package building.sum.report.listener;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,11 @@ public class SumReportServiceListener {
 
 	private final PDFReportService pdfReportService;
 
+	private static final Logger log = LogManager.getLogger();
+
 	@RabbitListener(queues = { "${daily-after-market-queue}" })
 	public void generateReport(String userJoinKey) {
+		log.info("Received report generation request for user - {}", userJoinKey);
 		pdfReportService.generateAfterMarketPdfReport(userJoinKey);
 	}
 
